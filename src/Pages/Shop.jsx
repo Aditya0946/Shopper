883a72a2e231ea92b8c4dbad939a1c00be505be4
Aditya Singh <78608794+Aditya0946@users.noreply.github.com>
2024-08-06 23:50:59 +0530

    Update - Home Page

---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,17 @@
+import Hero from "../Components/Hero/Hero";
+import Popular from "../Components/Popular/Popular";
+import Offers from "../Components/Offers/Offers";
+import NewCollections from "../Components/NewCollections/NewCollections";
+
+const Shop = () => {
+  return (
+    <div>
+      <Hero />
+      <Popular />
+      <Offers />
+      <NewCollections />
+    </div>
+  );
+};
+
+export default Shop;
